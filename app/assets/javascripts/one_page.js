712a79dc61911c19ec1185d7bbe84220fcda9638
John <johnbdonner@gmail.com>
2015-02-27 13:23:35 -0500

    animations

--- app/assets/javascripts/one_page.js
+++ app/assets/javascripts/one_page.js
@@ -78,8 +78,31 @@ $(document).ready(function() {
 		}
 	});
 
+	/*** 	Animations		***/
+	var $window = $(window), 
+		win_height_padded = $window.height() * 1.1, 
+		isTouch = Modernizr.touch;
 
+	$window.on('scroll', revealOnScroll);
 
+	function revealOnScroll() {
+	    var scrolled = $window.scrollTop();
+	    $(".revealOnScroll:not(.animated)").each(function () {
+	 	   	var $this     = $(this),
+			offsetTop = $this.offset().top;
+
+			if (scrolled + win_height_padded > offsetTop) {
+				if ($this.data('timeout')) {
+					window.setTimeout(function(){
+						$this.css('visibility', 'visible')
+						$this.addClass('animated ' + $this.data('animation'));
+					}, parseInt($this.data('timeout'),10));
+				} else {
+					$this.addClass('animated ' + $this.data('animation'));
+				}
+			}
+		});
+	};
 
 
 });
